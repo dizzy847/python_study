b94e5a041d576593a93f4a58eb6f6a720dea94e4
dizzy847 <maratsaidov847@gmail.com>
2025-04-11 13:45:43 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Comment
+# print('Результат:', 5 // 3) // округление к меньшему
+# print('Результат:', min(5, 6, 7, 9, -1)) результат -1
+# print('Результат:', max(5, 6, 7, 9, -1)) результат 9
+# print('Результат:', abs(-5)) результат 5
+# print('Результат:', pow(5, 3)) возведение в степень
+# print('Результат:', round(5 / 3)) округление к ближайшему
+# print("результат:", 8, 7, sep='', end='!\n') \n выводит со след строки
+# print('Second \' \t \\ l\ni\nn\ne') \t это таб
+
+# input('Введите возраст: ')
+
+# bool True or False
+# float числа с точкой
+# string любые слова
+number = 5 # int
+digit = -4.44545
+word = 'Резльтат:'
+boolean = False
+
+str_num = '5'
+
+# print(word, str(digit))
+print(number + int(str_num))
+print(word + str(number + int(str_num)))
+del number
---
+++ main2.py
@@ -0,0 +1,12 @@
+num1 = int(input('Введите первое число: '))
+
+num2 = int(input('Введите второе число: '))
+
+num1 +=5 # добавление к
+
+print('Result:', num1 + num2)
+print('Result:', num1 - num2)
+print('Result:', num1 / num2)
+print('Result:', num1 * num2)
+print('Result:', num1 ** num2)
+print('Result:', num1 // num2)
