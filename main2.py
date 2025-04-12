b87e05f34f6cf51f33c01c866ab8c41f053b69bd
drffh <maratsaidov@847@gmail.com>
2025-04-12 11:07:49 +0300

    ниче

--- main2.py
+++ main2.py
@@ -1,16 +1,19 @@
-num1 = int(input('Введите первое число: '))
+# num1 = int(input('Введите первое число: '))
 
-num2 = int(input('Введите второе число: '))
+# num2 = int(input('Введите второе число: '))
 
-num1 +=5 # добавление к
+# num1 +=5 # добавление к
 
-print('Result:', num1 + num2)
-print('Result:', num1 - num2)
-print('Result:', num1 / num2)
-print('Result:', num1 * num2)
-print('Result:', num1 ** num2)
-print('Result:', num1 // num2)
-print('Result:', num1 // num2)
-print('Result:', num1 // num2)
-print('Result:', num1 // num2)
+# print('Result:', num1 + num2)
+# print('Result:', num1 - num2)
+# print('Result:', num1 / num2)
+# print('Result:', num1 * num2)
+# print('Result:', num1 ** num2)
+# print('Result:', num1 // num2)
+# print('Result:', num1 // num2)
+# print('Result:', num1 // num2)
+# print('Result:', num1 // num2)
 
+data = input('Введите слово:')
+number = 5 if data == 'Five' else 0 
+print(number)
